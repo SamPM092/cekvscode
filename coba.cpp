48851ce4f078db4ed1c2a60dca9c661e5b6a09e1
SamPM092 <sammich4294@gmail.com>
2020-10-28 13:11:21 +0700

    s

---
+++ coba.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+
+    cout << "masukkan a dan b";
+    cin >> a >> b;
+    cout << "\nMenjadi : " << a + b;
+
+    return 0;
+}
